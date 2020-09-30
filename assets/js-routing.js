684db34aa2b1b89e5f20c51f6852caf599037e5f
Iab Foulds <ianfoulds@x-act.co.uk>
2020-09-30 13:28:16 +0100

    Build routing context and check for app_dev.php.

--- assets/js-routing.js
+++ assets/js-routing.js
@@ -10,5 +10,16 @@ if(elem) {
     routes = JSON.parse(elem.attributes['data-routing-data'].value);
 }
 
+let context = {
+    scheme: window.location.protocol,
+    host: window.location.hostname,
+    port: window.location.port,
+    base_url: '',
+    routes: routes,
+};
+if (window.location.pathname.startsWith('/app_dev.php')) {
+    context.base_url = '/app_dev.php';
+}
+
 window.Routing = new Router();
-window.Routing.setRoutes(routes);
+window.Routing.setRoutingData(context);
